0dd7e82be6cfa9a2057abf9711fc7af430376f81
Jason <1lanncontact@gmail.com>
2014-04-08 21:46:18 +0800

    Before import

---
+++ SimpleChat/SimpleChat/RadarViewController.h
@@ -0,0 +1,15 @@
+//
+//  RadarViewController.h
+//  SimpleChat
+//
+//  Created by Jason Chu on 4/8/14.
+//  Copyright (c) 2014 Jason Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RadarViewController : UIViewController
+
+
+
+@end
---
+++ SimpleChat/SimpleChat/RadarViewController.m
@@ -0,0 +1,38 @@
+//
+//  RadarViewController.m
+//  SimpleChat
+//
+//  Created by Jason Chu on 4/8/14.
+//  Copyright (c) 2014 Jason Chu. All rights reserved.
+//
+
+#import "RadarViewController.h"
+
+@interface RadarViewController ()
+
+@end
+
+@implementation RadarViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
