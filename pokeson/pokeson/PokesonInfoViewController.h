1dca9627b783798937ebb259564118b713d4d282
Jason <1lanncontact@gmail.com>
2014-04-20 12:21:58 +0800

    Small changes

---
+++ pokeson/pokeson/PokesonInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PokesonInfoViewController.h
+//  pokeson
+//
+//  Created by Jason Chu on 4/18/14.
+//  Copyright (c) 2014 Jason Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PokesonInfoViewController : UIViewController
+
+@end
